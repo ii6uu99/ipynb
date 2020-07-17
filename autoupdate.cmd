a676698471a9bc2dc3ee8030b09d72cde3216208
ii6uu99 <ii6uu99@163.com>
2020-07-17 17:52:36 +0800

    Auto Update

---
+++ autoupdate.cmd
@@ -0,0 +1,23 @@
+@echo off
+git config --global core.autocrlf false
+for /F "delims=?" %%i in ("%cd%") do set folder=%%~nxi
+set url=git@github.com:ii6uu99/%folder%.git
+
+if exist .git (
+git add .
+git commit -m "Auto Update"
+:: origin master
+git pull
+
+) else (
+
+
+git init
+git remote add github %url%
+git pull origin master
+git add .
+git commit -m init
+)
+git push -u origin master 
+
+pause
